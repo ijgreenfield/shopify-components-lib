37e969ff9e0264650a96aabeb594aa2c65d1d348
Ian Greenfield <ian.greenfield@heydayskincare.com>
2022-06-28 10:53:08 -0500

    material import adjustments

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Image } from '@builder.io/react';
 import { Parallax, Background } from 'react-parallax';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 export const Hero = props => {
   const { image, title, parallaxStrength, buttonLink, buttonText, height, darkMode } = props;
